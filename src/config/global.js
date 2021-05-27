cf79ef9d60222a92529d320031afc6abeb5dee77
Luis Gabriel Urueta Alvarez <lgurueta@misena.edu.co>
2021-05-27 09:48:20 -0500

    Hallazgos v2

--- src/config/global.js
+++ src/config/global.js
@@ -271,8 +271,8 @@ export const creditos = {
     {
       nombre: 'Tania Yaneth Ramírez Benítez',
       cargo: 'Soporte organizacional',
-      centro: 'Centro de Comercio y Servicios',
-      regional: 'Regional Tolima',
+      centro: 'Centro Industrial del Diseño y la Manufactura',
+      regional: 'Regional Santander',
     },
     {
       nombre: 'Claudia Patricia Sánchez Peñaranda',
@@ -300,12 +300,6 @@ export const creditos = {
       centro: 'Centro para la Industria de la Comunicación Gráfica',
       regional: 'Regional Distrito Capital',
     },
-    {
-      nombre: 'María Isabel Román Rueda',
-      cargo: 'Revisora de estilo',
-      centro: 'Centro Industrial del Diseño y la Manufactura',
-      regional: 'Regional Santander',
-    },
   ],
   desarrolloProducto: [
     {
@@ -318,12 +312,12 @@ export const creditos = {
     },
     {
       nombre: [
+        'Gilberto Junior Rodríguez Rodríguez',
+        'John Jairo Archiniegas Gonzáles',
+        'María Carolina Tamayo López',
         'María Natalia Maldonado Delgado',
+        'Wilson Andrés Arenales Cáceres',
         'Zuleidy Maria Ruiz Torres',
-        'Wilson Arenales ',
-        'Junior Rodriguez',
-        'John Archiniegas',
-        'Carolina Tamayo',
       ],
       cargo: 'Diseño web y Producción audiovisual',
     },
@@ -344,7 +338,7 @@ export const creditos = {
     {
       nombre: 'Milady Tatiana Villamil Castellanos',
       cargo: 'Validación y vinculación en plataforma LMS',
-      centro: 'Centro de comercio y servicios',
+      centro: 'Centro de Comercio y Servicios',
       regional: 'Regional Tolima',
     },
   ],
